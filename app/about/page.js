860b27732b482f3adad84cb145de82458e61391e
Raamponsah <raamponsah@gimpa.edu.gh>
2024-08-07 17:19:21 +0000

    next react beginings

--- app/about/page.js
+++ app/about/page.js
@@ -1,16 +1,12 @@
 export default function Page(){
-
-
-  return (
-    <div className="bg-fuchsia-400 p-20">
-      <h1>Welcome to The Flying Baker!</h1>
-      <nav className="flex justify-between">
+    return <div className="bg-fuchsia-500">
+    <h1>About Us</h1>
+    <nav className="flex justify-between">
         <a className="underline text-white" href="/">Home</a>
         <a className="underline text-white" href="/about">About</a>
         <a className="underline text-white" href="/products">Products</a>
         <a className="underline text-white" href="/contact">Contact</a>
       </nav>
-      <p>This is a simple example of a Next.js app.</p>
+    <p>This is about us page</p>
     </div>
-  )
 }
